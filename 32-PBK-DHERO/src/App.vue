e576e22e9f87184bb4bba4e893779461f171abec
Pipoy <rafi67818@gmail.com>
2025-05-09 02:15:10 +0700

    feat: container List Kegiatan, button input list, button add list

---
+++ 32-PBK-DHERO/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed } from 'vue'
+</script>
+
+<template>
+  <div class="container">
+    
+    <h1 class="judul">List Kegiatan</h1>
+
+    <div class="inputList">
+      <input
+        v-model="listbaru"
+        @keyup.enter="tambahlist"
+        class="inputKegiatan"
+        placeholder="Tambahkan kegiatan..."
+      />
+      <button @click="addTask" class="addButton">Tambah</button>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+</style>

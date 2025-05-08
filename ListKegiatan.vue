65dfba39def3b782f25808e73657a563c09f7ce7
DanielDhero <danieldherosaputra@gmail.com>
2025-05-09 02:18:34 +0700

    feat: button filter

---
+++ ListKegiatan.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, computed } from 'vue'
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="judul">List Kegiatan</h1>
+
+    <div class="inputList">
+      <input
+        v-model="listbaru"
+        @keyup.enter="tambahlist"
+        class="inputKegiatan"
+        placeholder="Tambahkan kegiatan..."
+      />
+      <button @click="addTask" class="addButton">Tambah</button>
+    </div>
+
+    <div class="filterTabel">
+      <label>Filter:</label>
+      <select v-model="filter" class="filter">
+        <option value="semua">Semua</option>
+        <option value="done">Selesai</option>
+        <option value="undone">Belum selesai</option>
+      </select>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+</style>
